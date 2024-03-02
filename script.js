aff41c66182134952b4d16d61edd397095eb708f
JennMatthias <160607757+JennMatthias@users.noreply.github.com>
2024-03-01 21:49:28 -0300

    Add files via upload
    JS de wordle con api

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 let intentos = 6;
-let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH'];
+let diccionario = ['APPLE', 'WHITE', 'WINGS', 'YOUTH', 'TWICE', 'GREEN', 'SUPER', 'COLOR', 'FLUID'];
 const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
 const button = document.getElementById("guess-button");
 console.log(palabra);
 button.addEventListener("click", intentar);
 
+let palabraApi = "";
+const UrlApi = "https://random-word-api.herokuapp.com/word?length=5&lang=es";
+console.log(palabraApi);
+fetch(UrlApi).then(response => response.json()).then(response=>{
+    palabraApi=response[0].toUpperCase();
+    console.log("API: ",palabraApi);
+}).catch(err =>{
+    console.log("Lista auxiliar: ",palabra);
+});
+
 function intentar() {
     const INTENTO = leerIntento();
-    if (INTENTO === palabra) {
-        mostrarPalabra(palabra, 'green'); // Mostrar la palabra en verde si es correcta
+    if (INTENTO === palabraApi) {
+        mostrarPalabra(palabraApi, 'green'); // Mostrar la palabra en verde si es correcta
         terminar("<h1>¡¡¡FELICIDADES!!!😀</h1>");
         return;
     }
     const ROW = document.createElement('div');
     ROW.className = 'row';
-    for (let i in palabra) {
+    for (let i in palabraApi) {
         const SPAN = document.createElement('span');
         SPAN.className = 'letter';
-        if (INTENTO[i] === palabra[i]) { //VERDE
+        if (INTENTO[i] === palabraApi[i]) { //VERDE
             SPAN.innerHTML = INTENTO[i];
             SPAN.style.backgroundColor = 'green';
-        } else if (palabra.includes(INTENTO[i])) { //AMARILLO
+        } else if (palabraApi.includes(INTENTO[i])) { //AMARILLO
             SPAN.innerHTML = INTENTO[i];
             SPAN.style.backgroundColor = 'yellow';
         } else {      //GRIS
@@ -50,14 +60,14 @@ function terminar(mensaje) {
     contenedor.innerHTML = mensaje;
 }
 
-function mostrarPalabra(palabra, color) {
+function mostrarPalabra(palabraApi, color) {
     const ROW = document.createElement('div');
     ROW.className = 'row';
-    for (let i in palabra) {
+    for (let i in palabraApi) {
         const SPAN = document.createElement('span');
         SPAN.className = 'letter';
-        SPAN.innerHTML = palabra[i];
-        SPAN.style.backgroundColor = "pink"; // Establecer el color
+        SPAN.innerHTML = palabraApi[i];
+        SPAN.style.backgroundColor = "#ba0c51";
         ROW.appendChild(SPAN);
     }
     document.getElementById("grid").appendChild(ROW);
